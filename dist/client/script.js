9d234ffd4bb229b08805334855d144cc940d9629
Colin Hu <colinhu@Owens-MacBook-Pro.local>
2026-07-22 16:24:22 -0400

    Restore idle pulse before globe zoom

--- dist/client/script.js
+++ dist/client/script.js
@@ -224,6 +224,7 @@
     if (frontRect) {
       var frontRunway = Math.max(1, frontRect.height - vh);
       frontJourneyProgress = clamp01(-frontRect.top / frontRunway);
+      frontJourney.classList.toggle("is-engaged", frontJourneyProgress > 0.006);
       var globeMorphP = clamp01(frontJourneyProgress / 0.18);
       var globeRevealP = smoother((frontJourneyProgress - 0.14) / 0.08);
       globeP = smoother((frontJourneyProgress - 0.22) / 0.38);
